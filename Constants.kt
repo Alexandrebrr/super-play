0dfcf9eb4ac2ff8c25a1f7da4f50cb3e9c0ad064
Alexandrebrr <corteis@gmail.com>
2026-08-11 22:31:43 -0300

    Delete Constants.kt

--- Constants.kt
+++
@@ -1,7 +0,0 @@
-
-package com.supplay.player.utils
-object Constants {
-    const val API_BASE_URL = "https://supplay.online/api/"
-    const val ACTIVATION_URL = "supplay.online/ativar"
-    const val APP_NAME = "Super Play"
-}
